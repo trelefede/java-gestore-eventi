f0d68094fb795b3221d349a51f69ddca9d7897cb
TreleaniFederico <treleani.federico@gmail.com>
2023-02-03 12:07:37 +0100

    add class Evento

--- src/org/lessons/java/gestore/eventi/Evento.java
+++ src/org/lessons/java/gestore/eventi/Evento.java
@@ -1,5 +1,98 @@
 package org.lessons.java.gestore.eventi;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Evento {
+
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati = 0;
+	private String dataForm = formatData(data);
+
+	public Evento(String titolo, LocalDate data, int postiTotali) throws Exception {
+		validateTitolo(titolo);
+		validateData(data);
+		validatePostiTotali(postiTotali);
+	}
+
+	// getters and setters
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) throws Exception {
+		validateTitolo(titolo);
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) throws Exception {
+		validateData(data);
+	}
+
+	public int getPostiTotali() {
+		return postiTotali;
+	}
+
+	public int getPostiPrenotati() {
+		return postiPrenotati;
+	}
+
+	// metodi prenota/disdici
+	public void prenota() throws Exception {
+		if (data.isBefore(LocalDate.now())) {
+			throw new Exception("I viaggi nel tempo ancora non esistono! L'evento non è più disponibile!");
+		} else if (postiPrenotati == postiTotali) {
+			throw new Exception("Posti terminati per questo evento");
+		}
+		this.postiPrenotati = this.postiPrenotati + 1;
+	}
+
+	public void disdici() throws Exception {
+		if (postiPrenotati == 0) {
+			throw new Exception("Nessuna prenotazione trovata");
+		} else if (data.isBefore(LocalDate.now())) {
+			throw new Exception("I viaggi nel tempo ancora non esistono! L'evento non è più disponibile!");
+		}
+		this.postiPrenotati = this.postiPrenotati - 1;
+	}
+
+	// validations
+	public void validateTitolo(String titolo) throws Exception {
+		if (titolo.isEmpty()) {
+			throw new Exception("Titolo obbligatorio!");
+		}
+		this.titolo = titolo;
+	}
+
+	public void validateData(LocalDate data) throws Exception {
+		if (data.isBefore(LocalDate.now())) {
+			throw new Exception("I viaggi nel tempo ancora non esistono! Inserisci una data futura");
+		}
+		this.data = data;
+	}
+
+	public void validatePostiTotali(int postiTotali) throws Exception {
+		if (postiTotali <= 0) {
+			throw new Exception("Numero posti totali non può essere inferiore a 1");
+		}
+		this.postiTotali = postiTotali;
+	}
+
+	// formattazione data
+	private String formatData(LocalDate data) {
+		DateTimeFormatter form = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+		String dataForm = data.format(form);
+		return dataForm;
+	}
+
+	@Override
+	public String toString() {
+		return "Data: " + dataForm + "\n" + "Evento: " + titolo;
+	}
 
 }
